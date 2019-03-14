87dd6360b31e3ea1f301e4c974a0c796febef721
Diana <di.tsevan@gmail.com>
2019-03-14 15:49:54 +0300

    initial setup

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.listen(3001, function () {
+  console.log('Example app listening on port 3001!');
+});
